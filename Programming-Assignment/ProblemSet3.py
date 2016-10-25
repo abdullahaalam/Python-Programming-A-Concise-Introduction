ebb9f836d37290dab250ba577402524daf6a1765
Abdullah A. Alam <abdullah.alam.omi@gmail.com>
2016-10-24 21:20:52 -0700

    Modified Python 3.7 Exerciess

--- Programming-Assignment/ProblemSet3.py
+++ Programming-Assignment/ProblemSet3.py
@@ -247,6 +247,10 @@ Solution starter:
 #%%
 import csv
 def problem3_7(csv_pricefile, flower):
-    pass # replace this pass (a do-nothing) statement with your code
+    infile = open(csv_pricefile)
+    reader = csv.reader(infile)
+    for row in reader:
+        if row[0] == flower:
+            print(row[1])
 
 #%%
