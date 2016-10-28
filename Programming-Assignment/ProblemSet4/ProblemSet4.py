358abd3bcb1fac85f7716309160be59493451576
Abdullah A. Alam <abdullah.alam.omi@gmail.com>
2016-10-27 23:57:08 -0700

    Added function to display a list of words, sort them, display them again

--- Programming-Assignment/ProblemSet4/ProblemSet4.py
+++ Programming-Assignment/ProblemSet4/ProblemSet4.py
@@ -29,8 +29,9 @@ firstline = ["Happy", "families", "are", "all", "alike;", "every", \
 #%%
 def problem4_1(wordlist):
     """ Takes a word list prints it, sorts it, and prints the sorted list """
-    pass # replace this pass (a do-nothing) statement with your code
-
+    print(wordlist)
+    wordlist.sort(key=str.lower)
+    print(wordlist)
     
 #%%
 """
